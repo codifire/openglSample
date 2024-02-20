d8d9a0e81e00a643412d47175167f0a14cd5c255
Newton SSH <igamepro1990@gmail.com>
2024-02-20 22:35:14 +0100

    added important files

--- CommonGL.h
+++ CommonGL.h
@@ -10,6 +10,19 @@
 #include <algorithm>
 #include <chrono>
 #include <cassert>
+#include <stdexcept>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <limits>
+#include <array>
+#include <optional>
+#include <set>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "CShader.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
