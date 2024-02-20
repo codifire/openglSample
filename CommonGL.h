f8f8b72354e17f8ca0200b1f8ba6a856f720242f
Newton SSH <igamepro1990@gmail.com>
2024-02-20 22:27:07 +0100

    first commit

---
+++ CommonGL.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+#include <fstream>
+#include <memory>
+#include <algorithm>
+#include <chrono>
+#include <cassert>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
